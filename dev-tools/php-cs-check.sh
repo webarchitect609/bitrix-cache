0ba426062c5e596d010664f03601eb162070670c
Sergey Gripinskiy <web-architect@mail.ru>
2021-08-28 08:46:16 +0300

    ci: return config option

--- dev-tools/php-cs-check.sh
+++ dev-tools/php-cs-check.sh
@@ -20,6 +20,7 @@ fi
 echo "EXTRA_ARGS: ${EXTRA_ARGS}"
 
 vendor/bin/php-cs-fixer fix \
+                            --config=.php-cs-fixer.dist.php \
                             --ansi \
                             --dry-run \
                             --stop-on-violation \
